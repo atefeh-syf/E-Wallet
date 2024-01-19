c2205f8923054db589c0c39e4c556d193187a0ca
atefeh-syf <atefe.s.77218@gmail.com>
2024-01-19 19:49:38 +0330

    withdraw from wallet

--- src/api/handlers/wallet.go
+++ src/api/handlers/wallet.go
@@ -39,9 +39,49 @@ func (h *WalletHandler) Deposit(w http.ResponseWriter, r *http.Request) {
 }
 
 func (h *WalletHandler) Withdraw(w http.ResponseWriter, r *http.Request) {
-
+	req := new(dto.TransactionAction)
+	err := json.NewDecoder(r.Body).Decode(&req)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+	s := services.GetWalletService()
+	result, err := s.Withdraw(req)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+	// Convert the result to JSON
+	res, err := json.Marshal(result)
+	if err != nil {
+		http.Error(w, "Failed to marshal response", http.StatusInternalServerError)
+		return
+	}
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write(res)
 }
 
 func (h *WalletHandler) ForceWithdraw(w http.ResponseWriter, r *http.Request) {
-
+	req := new(dto.TransactionAction)
+	err := json.NewDecoder(r.Body).Decode(&req)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+	s := services.GetWalletService()
+	result, err := s.Withdraw(req)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+	// Convert the result to JSON
+	res, err := json.Marshal(result)
+	if err != nil {
+		http.Error(w, "Failed to marshal response", http.StatusInternalServerError)
+		return
+	}
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write(res)
 }
--- src/services/wallet_service.go
+++ src/services/wallet_service.go
@@ -80,33 +80,75 @@ func (s *WalletService) FindWalletByUserId(userId int) (models.Wallet, error) {
 }
 
 func (s *WalletService) Withdraw(req *dto.TransactionAction) (*dto.TransactionResult, error) {
-	return &dto.TransactionResult{
-		Wallet: models.Wallet{
-			BaseModel:   models.BaseModel{},
-			Name:        "",
-			Type:        "",
-			Balance:     0,
-			Slug:        "",
-			Description: "",
-			UserId:      0,
-			User:        models.User{},
-		},
-		Result: false,
-	}, nil
+	result := true
+	wallet, err := s.FindWalletByUserId(req.UserId)
+	if err != nil {
+		return nil, err
+	}
+
+	channel := make(chan models.DBResponse) //channel for db response
+
+	s.WaitGroup.Add(1)
+	previousBalance := wallet.Balance
+	wallet.Balance = wallet.Balance - req.Amount
+
+	if wallet.Balance < 0 {
+		panic("wallet balance  don't enough")
+	}
+
+	walletBalanceUpdate := &dto.WalletBalanceUpdate{
+		Amount:          req.Amount,
+		PreviousBalance: previousBalance,
+		Type:            models.Withdraw,
+		Wallet:          wallet,
+	}
+	go s.Repository.UpdateWalletBalance(walletBalanceUpdate, channel)
+
+	WaitAndCloseChannel(s.WaitGroup, channel)
+
+	//get data from channel
+	for c := range channel {
+		wallet = c.Data.(models.Wallet)
+		err = c.Error
+	}
+
+	if err != nil {
+		result = false
+	}
+	return &dto.TransactionResult{Wallet: wallet, Result: result}, err
 }
 
 func (s *WalletService) ForceWithdraw(req *dto.TransactionAction) (*dto.TransactionResult, error) {
-	return &dto.TransactionResult{
-		Wallet: models.Wallet{
-			BaseModel:   models.BaseModel{},
-			Name:        "",
-			Type:        "",
-			Balance:     0,
-			Slug:        "",
-			Description: "",
-			UserId:      0,
-			User:        models.User{},
-		},
-		Result: false,
-	}, nil
+	result := true
+	wallet, err := s.FindWalletByUserId(req.UserId)
+	if err != nil {
+		return nil, err
+	}
+
+	channel := make(chan models.DBResponse) //channel for db response
+
+	s.WaitGroup.Add(1)
+	previousBalance := wallet.Balance
+	wallet.Balance = wallet.Balance - req.Amount
+
+	walletBalanceUpdate := &dto.WalletBalanceUpdate{
+		Amount:          req.Amount,
+		PreviousBalance: previousBalance,
+		Type:            models.Withdraw,
+		Wallet:          wallet,
+	}
+	go s.Repository.UpdateWalletBalance(walletBalanceUpdate, channel)
+
+	WaitAndCloseChannel(s.WaitGroup, channel)
+
+	//get data from channel
+	for c := range channel {
+		wallet = c.Data.(models.Wallet)
+		err = c.Error
+	}
+
+	if err != nil {
+		result = false
+	}
+	return &dto.TransactionResult{Wallet: wallet, Result: result}, err
 }
